1e05eb2c7efbf1dcc012a66d1c953ae2f5d01dac
marcinbinkowski <marcin.binkowski@onet.eu>
2018-09-27 01:25:23 +0200

    move coordinate method to BotManager. Add sys.exiton popup error. Alpha version

--- EmailCreator/EmailCreatorBot.py
+++ EmailCreator/EmailCreatorBot.py
@@ -4,10 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import FakeDataGenerator
 from time import sleep
 import re
-
+import FakeDataGenerator
 
 class EmailCreatorBot:
 
@@ -21,7 +20,7 @@ class EmailCreatorBot:
         self.driver = webdriver.Chrome()
         self.element = ""
         self.sms = ""
-
+        self.stop = False
         self.driver.get("https://catchsms.com/")
 
     def registration_focus(self, element, data):
@@ -43,10 +42,10 @@ class EmailCreatorBot:
         self.driver.find_element_by_tag_name("body").send_keys(Keys.ESCAPE)
         sleep(1)
         self.phone_number = self.driver.find_element_by_xpath(
-            "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/h6[1]").text
+            "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/h6[1]").text
 
     def get_sms(self):
-        self.driver.find_element_by_xpath("//div[@class='wrap']//div[2]//div[1]//a[1]").click()
+        self.driver.find_element_by_xpath("//div[@class='wrap']//div[3]//div[1]//a[1]").click()
         regex_data = self.driver.find_element_by_xpath(
             "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]").text
         self.sms = re.search('\d+|$', regex_data).group()
@@ -79,28 +78,20 @@ class EmailCreatorBot:
             "//input[@id='phone']"), self.phone_number)
         sleep(0.5)
         self.driver.find_element_by_xpath(
-            "//div[@class='registration__send-code show-block']//button[@type='button']").click()
+                "//div[@class='registration__send-code show-block']//button[@type='button']").click()
+        sleep(2)
+        try:
+            if self.driver.find_element_by_css_selector("div.layout:nth-child(1) div.layout-inner div.grid div.main-container main.registration__wrapper,.utilityfocus.registration__wrapper_desktop div.registration__block form.registration__form.registration__form_desktop div.human-confirmation-wrapper.can-switch:nth-child(4) div.phone__confirm-wrapper div.form__field.form__field_filled.field__error:nth-child(1) div.reg-field__popup div.form__popup-error > div.error-message").is_displayed() == True:
+                self.driver.close()
+                self.driver.switch_to.window(self.driver.window_handles[0])
+                self.driver.close()
+                self.stop = True
+        except:
+            None
 
 
-    def coordinate(self):
-        self.get_phone_number()
-        self.fill_out_forms()
-        sleep(2)
-        self.driver.switch_to.window(self.driver.window_handles[0])
-        sleep(3)
-        self.get_sms()
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        self.registration_focus(self.driver.find_element_by_xpath(
-            "//input[@id='phoneCode']"), self.sms)
-        sleep(2)
-        self.driver.find_element_by_xpath(
-            "//button[@type='submit']").click()
-        sleep(2)
-        self.driver.find_element_by_xpath(
-            "/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/main[1]/div[1]/div[1]/div[1]/form[1]/div[4]/div[1]/div[2]/div[1]/button[1]")
-        sleep(3)
-        self.eula_focus(self.driver.find_element_by_css_selector("div.layout:nth-child(1) div.layout-inner div.grid div.main-container main.registration__wrapper,.utilityfocus.registration__wrapper_desktop div.registration__block form.registration__form.registration__form_desktop div.form__submit:nth-child(5) div.eula-popup-wrapper.eula-popup__show div.eula-popup div.t-eula-accept > button.button2.button2_size_m.button2_theme_action.button2_width_max:nth-child(3)"))
-        sleep(4)
+
+
 
 if __name__ == "__main__":
     data = FakeDataGenerator.FakeDataGenerator().return_data()
