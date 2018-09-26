b39f584a6e5477dde409501b048ee5885bacbf77
marcinbinkowski <marcin.binkowski@onet.eu>
2018-09-26 05:54:21 +0200

    somehow working bot

--- EmailCreator/EmailCreatorBot.py
+++ EmailCreator/EmailCreatorBot.py
@@ -16,41 +16,42 @@ class EmailCreatorBot:
         self.surname = surname
         self.password = password
         self.login = login
-        self.phone_number = "234234"
+        self.phone_number = ""
 
         self.driver = webdriver.Chrome()
-        self.actions = ActionChains(self.driver)
         self.element = ""
-        self.regex_data = ""
         self.sms = ""
-        # self.driver.get('https://passport.yandex.com/registration/mail?from=mail&require_'
-        #            'hint=1&origin=hostroot_homer_reg_com&retpath'
-        #            '=\https%3A%2F%2Fmail.yandex.com%2F&backpath='
-        #            'https%3A%2F%2Fmail.yandex.com%3Fnoretpath%3D1')
+
+        self.driver.get("https://catchsms.com/")
 
     def registration_focus(self, element, data):
-        self.actions = ActionChains(self.driver)
-        self.actions.move_to_element(element)
-        self.actions.click()
-        self.actions.send_keys(str(data))
-        self.actions.perform()
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element)
+        actions.click()
+        actions.send_keys(str(data))
+        actions.perform()
 
     def get_phone_number(self):
-        self.driver.execute_script('''window.open("https://catchsms.com/","_blank");''')
-        self.driver.get("https://catchsms.com/")
         sleep(3)
         self.driver.find_element_by_tag_name("body").send_keys(Keys.ESCAPE)
         sleep(1)
         self.phone_number = self.driver.find_element_by_xpath(
             "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/h6[1]").text
+        print(self.phone_number)
 
     def get_sms(self):
         self.driver.find_element_by_xpath("//div[@class='wrap']//div[2]//div[1]//a[1]").click()
-        self.regex_data = self.driver.find_element_by_xpath(
+        regex_data = self.driver.find_element_by_xpath(
             "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]").text
-        self.sms = re.search('\d+|$', self.regex_data).group()
+        self.sms = re.search('\d+|$', regex_data).group()
+        print(self.sms)
 
     def fill_out_forms(self):
+        self.driver.execute_script(
+            '''window.open("https://passport.yandex.com/registration/mail?from=mail&require\
+            _hint=1&origin=hostroot_homer_reg_com&retpath=https%3A%2F%2Fmail.yandex.\
+            com%2F&backpath=https%3A%2F%2Fmail.yandex.com%3Fnoretpath%3D1","_blank");''')
+        self.driver.switch_to.window(self.driver.window_handles[1])
         self.element = WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='lg-cc__button lg-cc__button_type_action']")))
         self.element.click()
@@ -66,10 +67,25 @@ class EmailCreatorBot:
         self.registration_focus(self.driver.find_element_by_xpath(
             "//input[@id='password_confirm']"), self.password)
         self.registration_focus(self.driver.find_element_by_xpath(
-            "//input[@id='phone']"), "12313123")
+            "//input[@id='phone']"), self.phone_number)
+        sleep(0.5)
+        self.driver.find_element_by_xpath(
+            "//div[@class='registration__send-code show-block']//button[@type='button']").click()
 
     def coordinate(self):
         self.get_phone_number()
+        self.fill_out_forms()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        sleep(3)
+        self.get_sms()
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.registration_focus(self.driver.find_element_by_xpath(
+            "//input[@id='phoneCode']"), self.sms)
+        self.driver.find_element_by_xpath(
+            "//div[@class='phone__code-btn']//button[@type='button']").click()
+        self.driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div[2]/div/main/div/div/div/form/div[4]/button").click()
+    
 
 if __name__ == "__main__":
     data = FakeDataGenerator.FakeDataGenerator().return_data()
